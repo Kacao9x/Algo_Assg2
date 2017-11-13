0c3e3ed2f8edf0afbba6aadb983f6e26c1499c40
Kacao Cao <kacao@iastate.edu>
2017-11-13 16:44:15 -0600

    (1) created addVertex and addEdge function to genrate the graph data structure for the text input (from the given files) (2) need to implemnent diameter and centrality

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -8,21 +8,62 @@
 //  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
 
 import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 
 public class GraphProcessor
 {
 	// other member fields and methods
+	private Map<String, ArrayList<String>> graph = new LinkedHashMap<>();
+	
 
 	// NOTE: graphData should be an absolute file path
 	public GraphProcessor(String graphData)
 	{
-		// implementation
+		try{
+			BufferedReader input = new BufferedReader(new FileReader(graphData));
+			//skim number of vertices
+			input.readLine();
+			//read edges
+			String[] chunks = input.readLine().split(" ");
+			String u = chunks[0];
+			String v = chunks[1];
+			addVertex(u);
+			addEdge(u,v);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private void addVertex(String v) {
+		if(v == null || graph.containsKey(v))
+			return;
+		
+		graph.put(v, new ArrayList<>());
+		
+	}
+	
+	
+	private void addEdge(String u, String v) {
+		if(u == null || v == null 
+				|| u != null && !graph.containsKey(u)
+				|| u != null && graph.containsKey(u))
+			return;
+		
+		graph.get(u).add(v);
 	}
 
 	public int outDegree(String v)
 	{
-		// implementation
+		if (v == null || !graph.containsKey(v))
+			return -1;
+		
+		return graph.get(v).size();
+		
 	}
 
 	public ArrayList<String> bfsPath(String u, String v)
