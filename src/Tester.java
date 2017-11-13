5ceca20b46c6e8867935c2c7671c12ac921341fb
Kacao Cao <kacao@iastate.edu>
2017-11-13 16:08:42 -0600

    create Test class: testing crawler

---
+++ src/Tester.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class Tester {
+	public static void main (String[] args ) throws IOException {
+		ArrayList<String> topics = new ArrayList<>();
+		topics.add("Iowa State");
+		topics.add("Cyclones");
+		WikiCrawler crawler = new WikiCrawler("/wiki/Complexity_theory", 
+				20, new ArrayList<>(), "WikiCC.txt");
+		crawler.crawl();
+		
+		//GraphProcessor graphPro = new GraphProcessor("Wiki.txt");
+	}
+}
