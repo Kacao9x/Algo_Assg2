11cd420be9282ddaf71eba0f70fa95f6f91287c1
Kacao Cao <kacao@iastate.edu>
2017-11-15 17:02:04 -0600

    merge with Long's work. Final work on Wikicrawler

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -23,16 +23,14 @@ public class WikiCrawler {
     private ArrayList<String> topics;
     private String fileName;
 
-    private Map <String, ArrayList<String>> graph = new LinkedHashMap<>();
-    private int requestCounter = 0;
-    private static final int MAX_REQUEST_COUNTER = 50; //delay after each 'max-request-counter' requests
+    //private Map <String, ArrayList<String>> graph = new LinkedHashMap<>();
+    
     
     public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName) throws IOException {
         this.seedURL = seedUrl;
         this.max = max;
         this.topics = topics;
         this.fileName = fileName;
-        requestCounter = 0;
     }
 
     // NOTE: extractLinks takes the source HTML code, NOT a URL
@@ -69,119 +67,89 @@ public class WikiCrawler {
         return !toCheck.contains("#") && !toCheck.contains(":");
     }
 
-    public void crawl() throws IOException {
+    public void crawl() throws IOException, InterruptedException {
         
-        //HashSet<String> tobeVisited = new HashSet<>();
+    	StringBuilder stringBuilder = new StringBuilder();
+        BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+        HashSet<String> tobeVisited = new HashSet<>();
         HashSet<String> visited = new HashSet<>();
         ArrayDeque<String> queue = new ArrayDeque<>();
-
-        //out.write(String.valueOf(max));
+        int request = 0;
         int numVisited = 0;
-        //String seedURL = this.seedURL;
-        //String content = getContent(seedURL);
-        queue.add(seedURL);
-        visited.add(seedURL);
-//        if (containTopics(content)) {
-//            tobeVisited.add(seedURL);
-//            queue.offer(seedURL);
-//            numVisited++;
-//        }
-        while (queue.size() != 0 && numVisited < max) {
-            seedURL = queue.poll();
-            graph.putIfAbsent(seedURL, new ArrayList<>());
-            visited.add(seedURL);
-            String content = getContent(seedURL);
-            numVisited++;
-            
-            if(!containTopics(content))
-            	continue;
-            
-            ArrayList<String> links = extractLinks(content);
-            
-            for(int i = 0; i < links.size() && i < max - 1; ++i) { //String u : links) {
-                String u = links.get(i);
-                if(!visited.contains(u)) {
-                    queue.add(u);
-                    visited.add(u);
 
-                    if(!graph.get(seedURL).contains(u)) {
-                        graph.get(seedURL).add(u);
-                    }
-                }
-            }
+        String seedURL = this.seedURL;
+        String content = getContent(seedURL);
+        if (containTopics(content)) {
+            tobeVisited.add(seedURL);
+            queue.offer(seedURL);
+            numVisited++;
+            request++;
         }
-            /*
+        while (queue.size() != 0) {
+            seedURL = queue.poll();
+            visited.add(seedURL);
+            content = getContent(seedURL);
+            request++;
+            if (request >= 50) {
+                request = 0;
+                Thread.sleep(3011);
+            }
+            ArrayList<String> links = extractLinks(content);
             for (String link : links) {
                 if (link.equals(seedURL))
                     continue;
                 if (numVisited < max) {
                     String contentURL = getContent(link);
+                    request++;
+                    if (request >= 50) {
+                        request = 0;
+                        Thread.sleep(3011);
+                    }
                     if (!visited.contains(link)) {
                         if (containTopics(contentURL)) {
                             tobeVisited.add(link);
                             queue.offer(link);
                             numVisited++;
-                            writeToFile(out, seedURL, link);
+                            stringBuilder.append(System.getProperty("line.separator"));
+                            stringBuilder.append(seedURL).append(" ").append(link);
                         }
                     } else {
-                        writeToFile(out, seedURL, link);
+                        stringBuilder.append(System.getProperty("line.separator"));
+                        stringBuilder.append(seedURL).append(" ").append(link);
                     }
                 } else {
                     if (tobeVisited.contains(link)) {
-                        writeToFile(out, seedURL, link);
+                        stringBuilder.append(System.getProperty("line.separator"));
+                        stringBuilder.append(seedURL).append(" ").append(link);
                     }
                 }
-            } */
-        try (BufferedWriter out = new BufferedWriter(new FileWriter(fileName))) {
-            out.write(String.valueOf(max));
-            for(String vertex : graph.keySet()) {
-                for (String edge : graph.get(vertex)) {
-                    out.newLine();
-                    out.write(vertex + " " + edge);
-                }
             }
-
         }
-            
+        stringBuilder.insert(0, numVisited);
+        out.write(stringBuilder.toString());
+        out.close();   
     }
 
     private String getContent(String seedURL) throws IOException {
     	
     	if(seedURL == null) return "";
     	
-    	requestCounter++;
-    	
-    	if(requestCounter == MAX_REQUEST_COUNTER) {
-    		try {
-    			Thread.sleep(3000);		//delay 3s
-    		} catch (InterruptedException e) {
-    			e.printStackTrace();
-    		}
-    		requestCounter = 0;			//reset counter
-    	}
-    	
-    	StringBuilder content = new StringBuilder();
+    	StringBuilder stringBuilder = new StringBuilder();
+        URL url = new URL(BASE_URL + seedURL);
+        InputStream inputStream;
         try {
-        	URL url = new URL(BASE_URL + seedURL);
-            InputStream inputStream = url.openStream();
-            
-            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
-            	String LinetoRead;
-                
-                while ((LinetoRead = bufferedReader.readLine()) != null) {
-                    content.append(LinetoRead).append("\n");
-                    content.append(System.getProperty("line.separator"));
-                }
-            }
-            
-        } catch (Exception e) {} //do something to handle exception here
-    	
-        
-        /* TAs recommend this to encapsulate the right content */
-    	int start = content.toString().indexOf("<p>");
-    	int end = content.toString().lastIndexOf("</p>");
-    	
-        return content.toString().substring(start, end);
+             inputStream = url.openStream();
+        }
+        catch (FileNotFoundException e) {
+            return "";
+        }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String toRead;
+        while ((toRead = bufferedReader.readLine()) != null) {
+            stringBuilder.append(toRead);
+            stringBuilder.append(System.getProperty("line.separator"));
+        }
+        return stringBuilder.toString();
     }
 
 
